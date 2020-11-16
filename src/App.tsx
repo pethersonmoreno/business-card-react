1fb776a51a233536dea57bbe6900600f47210570
Petherson Moreno <pethermoreno@gmail.com>
2020-11-16 12:09:24 -0300

    refactor: changed social icons to a subcomeponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 /* eslint-disable react/no-unescaped-entities */
 import React, { useEffect, useState } from 'react';
 import Divider from './components/Divider';
-import IconGithub from './components/IconGithub';
-import IconLinkedin from './components/IconLinkedin';
-import IconTwitter from './components/IconTwitter';
 import Bubble from './components/Bubble';
+import SocialSection, { TSocial } from './components/SocialSection';
 import './App.css';
 
-type TSocial = {
-  item: string;
-  link: string;
-};
 type TData = {
   name: string;
   presentation: string;
   location: string;
   social: TSocial[]
-};
-const compSocial = {
-  linkedin: IconLinkedin,
-  github: IconGithub,
-  twitter: IconTwitter,
 };
 
 function App() {
@@ -49,18 +38,7 @@ function App() {
           <Divider />
           <p>{data.location}</p>
           <Divider />
-          <p>Find me on:</p>
-          <div className="social-icons">
-            {data.social.map((itemSocial) => {
-              const IconComponent = (compSocial as any)[itemSocial.item];
-              if (!IconComponent) {
-                return null;
-              }
-              return (
-                <a key={itemSocial.item} href={itemSocial.link}><IconComponent /></a>
-              );
-            })}
-          </div>
+          <SocialSection social={data.social} />
         </>
       )}
     </div>
